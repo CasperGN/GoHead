dc80dc771c7937788df8f05a27afaf766883f7c2
CasperGN <whopsec@protonmail.com>
2020-04-14 13:58:07 -0400

    Added -secret flag to get urls/endpoints and possible keys by following links on / page

--- cmd/gohead/main.go
+++ cmd/gohead/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"regexp"
 	"strings"
@@ -21,6 +22,7 @@ var (
 	outdir  string
 	threads int
 	silent  bool
+	secrets bool
 )
 
 func init() {
@@ -30,6 +32,7 @@ func init() {
 	flag.StringVar(&outdir, "outdir", "", "Supply a directory to output the result to. Writes 1 file per supplied target.")
 	flag.IntVar(&threads, "threads", 5, "Number of threads")
 	flag.BoolVar(&silent, "silent", false, "Print header (default false).")
+	flag.BoolVar(&secrets, "secrets", false, "Search JavaScript files for keys, passwords or secrets (default false)")
 }
 
 func main() {
@@ -85,6 +88,15 @@ func main() {
 	}
 
 	intIP, _ := regexp.Compile(`(10(\.[1-9]{1}[0-9]{1,2}){3})|(172(\.[1-9]{1}[0-9]{1,2}){3})|(192(\.[1-9]{1}[0-9]{1,2}){3})`)
+	jsFiles, _ := regexp.Compile(`src="([a-zA-Z0-9\./]+.js)"`)
+	urlFind, _ := regexp.Compile(`(/[a-zA-Z0-9\.]+){2,10}`)
+	awsKey, _ := regexp.Compile(`"(AKIA[0-9A-Z]{16})"`)
+	awsSecKey, _ := regexp.Compile(`"([0-9a-zA-Z/+]{40})"`)
+	azureSub, _ := regexp.Compile(`"([a-z0-9]{32})"`)
+	mailGun, _ := regexp.Compile(`"(key-[0-9a-zA-Z]{32})"`)
+	fbKey, _ := regexp.Compile(`"([0-9]{13,17})"`)
+	fbSecKey, _ := regexp.Compile(`"([0-9a-f]{32})"`)
+	passwd, _ := regexp.Compile(`"((Pass:|pass:|Pass=|pass=|Password:|password:|Password=|password=)(.*[!@#$%^&*a-zA-Z0-9]))"`)
 
 	var waitGroup sync.WaitGroup
 	assets := make(chan string)
@@ -95,8 +107,65 @@ func main() {
 			for asset := range assets {
 				data := ""
 				var regMatch []string
+				urlMap := make(map[string]string)
+				keysFound := make(map[string]map[string]string)
 				body, headers, target := gohead.Probe(asset)
 				if len(headers) > 0 {
+					if secrets {
+						for _, match := range jsFiles.FindAllStringSubmatch(body, -1) {
+							//fmt.Println(body)
+							var matchedUrl = ""
+							if strings.HasPrefix(match[1], "/") {
+								matchedUrl += match[1]
+							} else {
+								matchedUrl += "/"
+								matchedUrl += match[1]
+							}
+							jsBody, err := http.Get(asset + matchedUrl)
+							if err != nil {
+								fmt.Printf("Error on GET for %s%s: %s", asset, match[1], err)
+								continue
+							}
+
+							jsFile, err := ioutil.ReadAll(jsBody.Body)
+							jsBody.Body.Close()
+							if err != nil {
+								fmt.Printf("Error on reading body from %s%s: %s", asset, match[1], err)
+								continue
+							}
+							//fmt.Println(string(jsFile))
+							for _, match := range urlFind.FindAllStringSubmatch(string(jsFile), -1) {
+								urlMap[asset+matchedUrl] = match[0]
+							}
+							for _, match := range awsKey.FindAllStringSubmatch(string(jsFile), -1) {
+								keysFound[asset+matchedUrl][match[1]] = "awskey"
+							}
+							for _, match := range awsSecKey.FindAllStringSubmatch(string(jsFile), -1) {
+								fmt.Println("AwsSecKey:")
+								fmt.Println(match[1])
+							}
+							for _, match := range azureSub.FindAllStringSubmatch(string(jsFile), -1) {
+								fmt.Println("Azure:")
+								fmt.Println(match[1])
+							}
+							for _, match := range mailGun.FindAllStringSubmatch(string(jsFile), -1) {
+								fmt.Println("MailGun:")
+								fmt.Println(match[1])
+							}
+							for _, match := range fbKey.FindAllStringSubmatch(string(jsFile), -1) {
+								fmt.Println("FbKey:")
+								fmt.Println(match[1])
+							}
+							for _, match := range fbSecKey.FindAllStringSubmatch(string(jsFile), -1) {
+								fmt.Println("FbSecKey:")
+								fmt.Println(match[1])
+							}
+							for _, match := range passwd.FindAllStringSubmatch(string(jsFile), -1) {
+								fmt.Println("Passwd:")
+								fmt.Println(match[1])
+							}
+						}
+					}
 					for _, match := range intIP.FindAllString(body, -1) {
 						regMatch = append(regMatch, match)
 					}
@@ -119,8 +188,10 @@ func main() {
 						data += key + ": " + option + "\n"
 					}
 				}
-				ips := "Internal IPs: "
+				ips := ""
 				if len(regMatch) > 0 {
+					data += "\n"
+					ips += "Internal IPs:"
 					for _, ip := range regMatch {
 						if strings.Contains(ips, ip) {
 							continue
@@ -131,6 +202,23 @@ func main() {
 					fmt.Printf(ips)
 				}
 				data += ips
+
+				if secrets {
+					if len(urlMap) > 0 {
+						data += "\nPossible endpoints:\n"
+						for key, value := range urlMap {
+							data += key + ": " + value + "\n"
+						}
+					}
+					if len(keysFound) > 0 {
+						data += "\nPossible keys:\n"
+						for key, value := range keysFound {
+							for k, v := range value {
+								data += v + ": " + k + " in " + key
+							}
+						}
+					}
+				}
 
 				if output {
 					filename := strings.Replace(strings.Split(asset, ":")[1], "/", "", -1)
